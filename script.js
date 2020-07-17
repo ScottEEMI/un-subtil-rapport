4c57f155f2779f682d3c0dad7540081896324917
pepkerESD <>
2020-07-17 12:54:55 +0200

    Est-ce que je suis un dieux ?

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 // EFFET DE pornup
-let indexPop = 0;
+let indexPop = -1;
 let timeR = 3 * 60000;
 let containerPop = $(".poporn");
 setInterval(function(){
-    containerPop.append("<div style='left: "+ft_rand(50)+"%;top:"+ft_rand(45)+"%;' class='border-pop'><div onclick='ft_remove(this.parentElement)' class='deletepop'>X</div><div class='background poporn-size poporn-"+ft_rand(6)+"'></div></div>");
+    containerPop.append("<div style='left: "+ft_rand(50)+"%;top:"+ft_rand(45)+"%;' class='border-pop'><div onclick='ft_remove(this.parentElement)' class='deletepop'>X - DELETE</div><div class='background poporn-size poporn-"+ft_rand(10)+"'></div></div>");
 }, timeR);
 
 function ft_remove(me) {
