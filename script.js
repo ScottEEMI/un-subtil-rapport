bd731a3d57e7f07374f5f0967e6de56c4aebe920
pepkerESD <>
2020-07-17 20:42:15 +0200

    lol 2minutes <3

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
 // EFFET DE pornup
 let indexPop = -1;
-let timeR = 2 * 600000;
+let timeR = 2 * 60000;
 let containerPop = $(".poporn");
 setInterval(function(){
+    console.log("coucou");
     containerPop.append("<div style='left: "+ft_rand(50)+"%;top:"+ft_rand(45)+"%;' class='border-pop'><div onclick='ft_remove(this.parentElement)' class='deletepop'>X - DELETE</div><div class='background poporn-size poporn-"+ft_rand(11)+"'></div></div>");
 }, timeR);
 
